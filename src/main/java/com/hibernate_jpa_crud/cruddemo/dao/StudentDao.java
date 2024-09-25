2138fa716abe9de242325222fbff76c22bf762e2
SarmadRehman02 <sarmad@devinnovates.com>
2024-09-25 18:14:48 +0500

    created a DAO(data-access-object) and its implementation for the Student

---
+++ src/main/java/com/hibernate_jpa_crud/cruddemo/dao/StudentDao.java
@@ -0,0 +1,9 @@
+package com.hibernate_jpa_crud.cruddemo.dao;
+
+import com.hibernate_jpa_crud.cruddemo.entity.Student;
+
+public interface StudentDao {
+
+
+    void save(Student theStudent);
+}
---
+++ src/main/java/com/hibernate_jpa_crud/cruddemo/dao/StudentDaoImpl.java
@@ -0,0 +1,31 @@
+package com.hibernate_jpa_crud.cruddemo.dao;
+
+import com.hibernate_jpa_crud.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class StudentDaoImpl implements StudentDao     {
+
+
+    // define field for entitiy manager
+    private EntityManager entityManager;
+
+
+    // injecting entitymanager using constructor injection
+    @Autowired
+    public StudentDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+
+
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+
+    }
+}
